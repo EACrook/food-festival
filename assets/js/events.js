4b600f0aa9901b6a6920034cbb611623729a0b9b
Emily Crook <Emily.A.Crook96@gmail.com>
2021-07-01 17:55:08 -0500

    image optimization complete

---
+++ assets/js/events.js
@@ -0,0 +1,9 @@
+require("bootstrap");
+const createEl = require("./domMethods");
+const createLoremIpsum = require("./helpers");
+
+$(document).ready( function() {
+    if (window.location.href.indexOf("events") > -1) {
+
+    }
+})
---
+++ assets/js/tickets.js
@@ -0,0 +1,8 @@
+require("bootstrap");
+const createEl = require("./domMethods");
+
+$(document).ready( function() {
+    if (window.location.href.indexOf("tickets") > -1) {
+
+    }
+})
